f8454659126900805fe3a35df3b08688f01c3803
Heaven7 <donshine723@gmail.com>
2023-04-24 15:08:35 +0800

    Update metartc-build.sh

--- FFmpeg-n4.3.3/metartc-build.sh
+++ FFmpeg-n4.3.3/metartc-build.sh
@@ -1,1 +1,3 @@
 ./configure --enable-libx264 --enable-gpl --extra-libs='-L/home/yang/FFmpeg-n4.3.3/metartc6 -lmetartccore6 -lpthread -lsrtp2 -lssl -lcrypto -ldl'
+#export CUR_DIR=$(pwd)/metartc6
+#./configure --enable-libx264 --enable-gpl --extra-libs='-L${CUR_DIR} -lmetartccore6 -lpthread -lsrtp2 -lssl -lcrypto -ldl'
